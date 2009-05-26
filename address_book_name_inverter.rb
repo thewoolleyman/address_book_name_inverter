c32a953df50f5a80820a57052eadb494218daf3e
Chad Woolley <thewoolleyman@gmail.com>
2009-05-25 17:34:33 -0700

    Works For Me

--- address_book_name_inverter.rb
+++ address_book_name_inverter.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 
 # Thanks to Wes: http://www.brokenbuild.com/blog/2009/05/09/cleaning-up-your-address-book-on-os-x-with-ruby/
+
+# TODO: This currently makes you pick twice for each dup, but you can kill it in the middle to start over.  Too lazy to finish it...
 require 'rubygems'
 require 'appscript'
  
@@ -8,14 +10,7 @@ include Appscript
  
 ab = app("Address Book")
  
-group_name = "Inverted Duplicate Names"
- 
-if ab.groups[group_name].eq(:missing_value)
-   puts "Creating a group named '#{group_name}'"
-   ab.make(:new => :group, :with_properties => {:name => group_name})
-end
-
-people = {}
+people = []
 
 # put in a hash and remove any trailing commas or spaces
 ab.people.get.each do |person|
@@ -23,29 +18,43 @@ ab.people.get.each do |person|
   last_name = person.last_name.get
   # reject one-name people
   unless last_name == :missing_value or first_name == :missing_value
-    people[person.id_.get] = {:first_name => first_name.chomp(',').strip, :last_name => last_name.chomp(',').strip}
+    people << {:first_name => first_name.chomp(',').strip, :last_name => last_name.chomp(',').strip, :person => person}
   end
 end
 
 inverted = []
-people.sort.each do |id1, names1|
-  people.sort.each do |id2, names2|
-    if names1[:first_name] == names2[:last_name] and names1[:last_name] == names2[:first_name]
-      inverted << [names1, names2, id1, id2]
+people.each do |values1|
+  people.each do |values2|
+    if values1[:first_name] == values2[:last_name] and values1[:last_name] == values2[:first_name]
+      inverted << [values1, values2]
     end
   end
 end
 
+puts "#{inverted.size} inverted names found."
+
 inverted.each do |pair|
-  puts "#{pair[0].values.join(' ')} | #{pair[1].values.join(' ')} | #{pair[2]} | #{pair[3]}\n"
+  first_name = pair[0][:first_name]
+  last_name = pair[0][:last_name]
+  person1 = pair[0][:person]
+  person2 = pair[1][:person]
+  puts "is #{first_name} the first name of #{first_name} #{last_name}?"
+  answer = gets
+  if answer.strip.downcase == 'n'
+    tmp = first_name
+    first_name = last_name
+    last_name = tmp
+  end
+  person1.first_name.set(first_name)
+  person1.last_name.set(last_name)
+  person2.first_name.set(first_name)
+  person2.last_name.set(last_name)
+  puts "#{person1.first_name.get} #{person1.last_name.get} #{person2.first_name.get} #{person2.last_name.get}"
+  ab.save_addressbook
 end
 
-puts "#{inverted.size} inverted names found."
- 
-# TODO
-# switch inverted ones - probably need manual choice to correctly pick first vs. last name
-# To write: wes.first_name.set('Joe')
- 
-# You need to save your changes, really.
-# ab.save_addressbook
-
+inverted.each do |values|
+  person1 = values[0][:person]
+  person2 = values[1][:person]
+  puts "#{person1.first_name.get} #{person1.last_name.get} #{person2.first_name.get} #{person2.last_name.get}"
+end
